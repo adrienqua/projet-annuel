cec8e44d29f000add243857190be1e75a50a4782
Harkansky <gregoire.wartner@lacatholille.fr>
2025-07-17 21:12:13 +0200

    Homepage + fix majuscule ProfileView + Footer + ajout d'un build aux fixtures

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/ui/Navbar.vue'
+import Footer from './components/ui/Footer.vue'
 </script>
 
 <template>
@@ -9,5 +10,6 @@ import Navbar from './components/ui/Navbar.vue'
     <div class="mx-auto container max-w-6xl px-4 pt-12">
       <RouterView />
     </div>
+    <Footer />
   </div>
 </template>
