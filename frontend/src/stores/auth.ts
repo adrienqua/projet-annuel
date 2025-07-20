112a1584a596ed0f88a5d8f405abb51b9e113bcb
denisi <simonpro.denis@gmail.com>
2025-07-21 00:41:41 +0200

    correctif pour la mise en ligne

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -2,13 +2,22 @@ import { reactive, ref } from 'vue'
 import { jwtDecode } from 'jwt-decode'
 import { getUser } from '@/services/AuthAPI'
 
+interface User {
+  id: string
+  email: string
+  [key: string]: any
+}
+
 const token = ref(localStorage.getItem('token'))
-let user = reactive({})
+const user = reactive<Partial<User>>({})
 
 if (token.value) {
   const decodedToken: any = jwtDecode(token.value)
-  user = await getUser(decodedToken.email)
-  if (user?.id) {
+  const fetchedUser = await getUser(decodedToken.email)
+
+  Object.assign(user, fetchedUser)
+
+  if (user.id) {
     localStorage.setItem('userId', user.id)
   }
 
@@ -17,7 +26,7 @@ if (token.value) {
 
 export function useAuth() {
   return {
-    user: user,
+    user,
     setToken(value: string) {
       token.value = value
       localStorage.setItem('token', value)
