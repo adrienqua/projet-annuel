703ab6c9d3db1991712471e96f636fd03b81d952
Denisi <simon.denis64@gmail.com>
2025-07-21 11:41:12 +0200

    test corectif

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -1,46 +1,36 @@
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import { jwtDecode } from 'jwt-decode'
 import { getUser } from '@/services/AuthAPI'
 import type { User } from '@/components/types/user'
 
-const token = ref<string | null>(localStorage.getItem('token'))
-const user = ref<Partial<User>>({})
+const token = ref(localStorage.getItem('token'))
+const user = reactive<Partial<User>>({})
 
 const initUser = async () => {
   if (token.value) {
-    try {
-      const decodedToken: any = jwtDecode(token.value)
-      const fetchedUser = await getUser(decodedToken.email)
-
-      if (fetchedUser?.id) {
-        user.value = fetchedUser
-        localStorage.setItem('userId', fetchedUser.id)
-      }
-    } catch (err) {
-      console.error('Erreur auth:', err)
-      clearToken()
+    const decodedToken: any = jwtDecode(token.value)
+    const fetchedUser = await getUser(decodedToken.email)
+    if (fetchedUser?.id) {
+      localStorage.setItem('userId', fetchedUser.id)
+      Object.assign(user, fetchedUser)
     }
+    console.log(user, 'user')
   }
 }
 
-const setToken = (value: string) => {
-  token.value = value
-  localStorage.setItem('token', value)
-}
-
-const clearToken = () => {
-  token.value = null
-  user.value = {}
-  localStorage.removeItem('token')
-  localStorage.removeItem('userId')
-}
+initUser()
 
 export function useAuth() {
   return {
-    token,
     user,
-    initUser,
-    setToken,
-    clearToken,
+    setToken(value: string) {
+      token.value = value
+      localStorage.setItem('token', value)
+    },
+    clearToken() {
+      token.value = null
+      localStorage.removeItem('token')
+      localStorage.removeItem('userId')
+    },
   }
 }
