e6631dd7415461695cf73f054b62f0b28b993e3c
Denisi <simon.denis64@gmail.com>
2025-07-21 11:13:47 +0200

    Debut de mise en place detail produit et affichage pour le comparatif

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -4,21 +4,25 @@ import { getUser } from '@/services/AuthAPI'
 import type { User } from '@/components/types/user'
 
 const token = ref(localStorage.getItem('token'))
-let user = reactive<Partial<User>>({})
+const user = reactive<Partial<User>>({})
 
-if (token.value) {
-  const decodedToken: any = jwtDecode(token.value)
-  user = await getUser(decodedToken.email)
-  if (user?.id) {
-    localStorage.setItem('userId', user.id)
+const initUser = async () => {
+  if (token.value) {
+    const decodedToken: any = jwtDecode(token.value)
+    const fetchedUser = await getUser(decodedToken.email)
+    if (fetchedUser?.id) {
+      localStorage.setItem('userId', fetchedUser.id)
+      Object.assign(user, fetchedUser)
+    }
+    console.log(user, 'user')
   }
-
-  console.log(user, 'user')
 }
+
+initUser()
 
 export function useAuth() {
   return {
-    user: user,
+    user,
     setToken(value: string) {
       token.value = value
       localStorage.setItem('token', value)
